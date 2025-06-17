55e4b492470292dff5240f2a4c902c2812d3fdd3
omilhano <alexmvfrancisco@gmail.com>
2025-06-17 15:13:50 +0100

    verification?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,30 @@
-import React from "react";
+import react from "react";
 import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./Navbar.css";
+import { useState } from "react";
 
 const NavbarComponents = () => {
+
+    const [expanded, setExpanded] = useState(false); // Controls expanded state
+
+    const handleNavClick = () => {
+        setExpanded(false); // Collapse the navbar after link click
+    };
+
     return (
-        <Navbar expand="lg" className="bg-white" sticky="top">
+        <Navbar expanded={expanded} expand="lg" className="bg-white" sticky="top">
             <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : true)} />
                 <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
                     <Nav className="m-auto">
-                        <Navbar.Brand as={Link} to="/">Carla Milhano</Navbar.Brand>
-                        <Nav.Link as={Link} to="/psicologiaepsicoterapia">Consultas de Psicologia</Nav.Link>
-                        <Nav.Link as={Link} to="/mindfulwalking">Passeios meditativos</Nav.Link>
-                        <Nav.Link as={Link} to="/eventos">Eventos</Nav.Link>
-                        <Nav.Link as={Link} to="/contactos">Contactos</Nav.Link>
+                        <Navbar.Brand as={Link} to="/" onClick={handleNavClick}>Carla Milhano</Navbar.Brand>
+                        <Nav.Link as={Link} to="/psicologiaepsicoterapia" onClick={handleNavClick}>Consultas de Psicologia</Nav.Link>
+                        <Nav.Link as={Link} to="/mindfulwalking" onClick={handleNavClick}>Passeios meditativos</Nav.Link>
+                        <Nav.Link as={Link} to="/eventos" onClick={handleNavClick}>Eventos</Nav.Link>
+                        <Nav.Link as={Link} to="/contactos" onClick={handleNavClick}>Contactos</Nav.Link>
                         <Nav.Link id="whatsapp-link"
                             href="https://wa.me/351962935461?text=Olá%20Carla%2C%20gostaria%20de%20marcar%20uma%20consulta."
                             target="_blank"
